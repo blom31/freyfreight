0b497b3bf2e70d466c4ee2c9880860c2a004da51
Olga Betancourt <blom31@gmail.com>
2026-05-04 22:59:34 -0300

    Revert "Se crean carpeta Layout para trabajar las capas publicas y el panel admin"
    This reverts commit v
    
    1c12faa989aedc7d1c83f102d6f8f974e7b53c04.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { PublicLayout } from "./layouts/PublicLayout/PublicLayaout";
-import { AdminLayout } from "./layouts/AdminLayout/AdminLayout";
+import { TopNavBar } from "./components/TopNavBar/TopNavbar";
 import { Hero } from "./components/Hero/Hero";
 import { Features } from "./components/Features/Features";
 import { Stats } from "./components/Stats/Stats";
 import { Cta } from "./components/Cta/Cta";
+import { Footer } from "./components/Footer/Footer";
 
 export function App() {
   return (
     <Router>
       <div className="app-container">
+        <TopNavBar />
+
         <main>
           <Routes>
-            <Route path="/" element={<PublicLayout />}></Route>
             <Route
-              index
+              path="/"
               element={
                 <>
                   <Hero />
+
                   <Features />
+
                   <Stats />
+
                   <Cta />
+
+                  <Footer />
                 </>
               }
             />
-            <Route />
-
-            {/* Rutas de Administración (usan AdminLayout) */}
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route
-                index
-                element={<h2>Dashboard Administrativo (Próximamente)</h2>}
-              />
-            </Route>
           </Routes>
         </main>
       </div>
